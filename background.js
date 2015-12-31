7c364ee9dffc8b6d1f7a4531e8695d793daf0bc2
Richard Waltman <rtwaltman@umail.ucsb.edu>
2015-12-31 02:26:08 -0800

    Removed unused files and formatting

--- background.js
+++ background.js
@@ -4,16 +4,16 @@
  *  - result: displays the icon in address bar to indicate script is active
  */
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [
-                new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: {
-                      urlEquals: 'https://my.sa.ucsb.edu/gold/StudentSchedule.aspx'
-                    }
-                })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {
+            urlEquals: 'https://my.sa.ucsb.edu/gold/StudentSchedule.aspx'
+          }
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
 });
